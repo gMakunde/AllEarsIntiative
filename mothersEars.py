60853cd8a042ce1dcd9012e268539c4e2f8221a2
Ubuntu <ubuntu@ip-172-31-47-224.ec2.internal>
2020-01-16 09:59:56 +0000

    fixed dashboard button bug that distored the other elements o the webpage

--- mothersEars.py
+++ mothersEars.py
@@ -3,14 +3,17 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route('/a')
+@app.route('/questions')
 def InitialQuestions():
     return flask.render_template('intitalquestions.html')
     
 @app.route('/')
+def Homepage():
+    return flask.render_template('homepage.html')
+
+@app.route('/dash')
 def Dashboard():
     return flask.render_template('dashboard.html')
-    
     
 app.run(
     host=os.getenv('IP', '0.0.0.0'),
